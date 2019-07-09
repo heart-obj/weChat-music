fad5ea310f519d13f1c35423f8507b377b1d631f
heart-obj <1135769417@qq.com>
2019-07-09 17:21:00 +0800

    up

--- pages/homePage/index.js
+++ pages/homePage/index.js
@@ -5,50 +5,51 @@ const app = getApp()
 Page({
   data: {
     motto: '音乐播放器',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    imgUrls: [
+      './img/1509030.jpg',
+      './img/1515000.jpg',
+      './img/1515403.jpg',
+      './img/1520124.jpg',
+      './img/1520134.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000,
+    circular: true,
+    classifyMusic: [
+      {
+        imgSrc: './img/track_radio_199_12_8.jpg',
+        text: '热门'
+      },
+      {
+        imgSrc: './img/20190709165132.jpg',
+        text: '流行'
+      },
+      {
+        imgSrc: './img/T005R600x600M000002CJKAY1LKpcz.jpg',
+        text: '摇滚'
+      },
+      {
+        imgSrc: './img/T006R300x300M000004bFmjW2PXSqF.jpg',
+        text: '民谣'
+      },
+      {
+        imgSrc: './img/20190709163624.jpg',
+        text: '古风'
+      },
+      {
+        imgSrc: './img/20190709164419.jpg',
+        text: '伤感'
+      },
+      {
+        imgSrc: './img/20190709165236.jpg',
+        text: '轻音乐'
+      },
+      {
+        imgSrc: './img/20190709165324.jpg',
+        text: '经典'
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    ]
   }
 })
